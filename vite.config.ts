265402f837916a14233fe3c00d5ae082bf2cf3b0
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-08-09 06:03:53 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/RaceNova-V2/",
+
   server: {
     host: true,
     port: 5173
   },
+
   build: {
     target: "es2022"
   }
